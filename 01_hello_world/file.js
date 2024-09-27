9c2f3a9ef1e650db84bcc06179789a39dde7ef15
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-09-27 22:36:43 +0500

    nodejs arcitacture understanding

--- 01_hello_world/file.js
+++ 01_hello_world/file.js
@@ -23,5 +23,24 @@ fs.writeFileSync("./test.txt", "Hey There");
 // fs.appendFileSync("./test.txt", `${Date.now()} Hey there\n`);
 // fs.cpSync("./test.txt", "copy.txt");
 // fs.unlinkSync("./copy.txt")
-console.log(fs.statSync("./test.txt"))
+// console.log(fs.statSync("./test.txt"));
 
+// how nodejs work || nodejs arcitacture => user request => Event Queue: (first in first out) => Event Loop: (pickup those request one by one, response if Async || non-blocking else if Sync || blocking code then theared pool ke pass blocking code chala jata he phir theared work karke response return kardeta he) aik theared pool ke pass 4 theared (worker) hote hen
+
+// example of blocking code
+// const result = fs.readFileSync("./test.txt", "utf-8");
+// console.log("1");
+// console.log(result);
+// console.log("2");
+
+// example of non-blocking code
+console.log("1");
+fs.readFile("./test.txt", "utf-8", (error, result) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(result);
+  }
+});
+
+console.log("2");
